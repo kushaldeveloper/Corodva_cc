43e3792657e7ac636fceb564ecff4b3ea9a60674
kushaldeveloper <43170976+kushaldeveloper@users.noreply.github.com>
2018-09-13 18:12:13 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Corodva_cc
---
+++ www/js/myFunc.js
@@ -0,0 +1,26 @@
+document.addEventListener("deviceready", onDeviceReady,false);
+		function onDeviceReady() {
+		alert("Device is starting");
+		cameraTakePicture();
+		}
+		function cameraTakePicture() { 
+		alert('camera is starting');
+		alert('camera:' + navigator.camera);
+		alert('camera:' + navigator.camera.getPicture);
+					   navigator.camera.getPicture(onSuccess, onFail, {  
+						  quality: 50, 
+						  destinationType: Camera.DestinationType.DATA_URL 
+					   });  
+					   
+					   function onSuccess(imageData) { 
+					   alert('sucess');
+						  var image = document.getElementById('myImage'); 
+						  image.src = "data:image/jpeg;base64," + imageData; 
+					   }  
+					   
+					   function onFail(message) { 
+						  alert('Failed because: ' + message); 
+					   } 
+					
+					}
+		
